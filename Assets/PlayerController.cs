f0b140a20f4c2525764817480aec42ff99c5176c
JrStarlord <97310506+JrStarlord@users.noreply.github.com>
2022-04-21 13:34:22 -0700

    basic slolum flight enabled

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float forwardSpeed = 25, strafeSpeed = 7, hovorSpeed = 5;
-    private float activeForwardSpeed, activeStrafeSpeed, activeHovorSpeed;
+    public float forwardSpeed = 25f, strafeSpeed = 7f, hoverSpeed = 5f;
+    private float activeForwardSpeed, activeStrafeSpeed, activeHoverSpeed;
+    private float forwardAcceleration = 2.5f, strafeAcceleration = 1f, hoverAccelereatoin = 2f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +17,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        activeForwardSpeed = Input.GetAxisRaw("Vertical") * forwardSpeed;
-        activeStrafeSpeed = Input.GetAxisRaw("Horizontal") * strafeSpeed;
-        activeHovorSpeed = Input.GetAxisRaw("Hovor") * hovorSpeed;
+        activeForwardSpeed = Mathf.Lerp(activeForwardSpeed, Input.GetAxisRaw("Vertical") * forwardSpeed, forwardAcceleration * Time.deltaTime);
+        activeStrafeSpeed = Mathf.Lerp(activeStrafeSpeed, Input.GetAxisRaw("Horizontal") * strafeSpeed, strafeAcceleration * Time.deltaTime);
+        activeHoverSpeed = Mathf.Lerp(activeHoverSpeed, Input.GetAxisRaw("Hover") * hoverSpeed, hoverAccelereatoin * Time.deltaTime);
 
         transform.position += transform.forward * activeForwardSpeed * Time.deltaTime;
+        transform.position += (transform.right * activeStrafeSpeed * Time.deltaTime) + (transform.up * activeHoverSpeed * Time.deltaTime);
     }
 }
